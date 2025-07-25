8187d631048bf6ea90bd9ef7222cbb637342a9d4
roygicheru <gicheru.k.roy@gmail.com>
2025-07-25 10:51:53 +0300

    new changes

--- ocsspring/src/main/java/com/team/onlinecatalogsystem/model/Product.java
+++ ocsspring/src/main/java/com/team/onlinecatalogsystem/model/Product.java
@@ -1,6 +1,6 @@
 package com.team.onlinecatalogsystem.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+// import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
